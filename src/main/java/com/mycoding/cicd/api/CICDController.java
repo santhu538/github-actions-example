c95e7479024bfb465b9205891b9801a96eee369b
Santhosh Kumar Boini <48869260+santhu538@users.noreply.github.com>
2023-04-22 16:21:23 -0400

    first commit

---
+++ src/main/java/com/mycoding/cicd/api/CICDController.java
@@ -0,0 +1,20 @@
+package com.mycoding.cicd.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/cicd")
+public class CICDController {
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to Github Actions";
+    }
+    /*
+    git init
+git add README.md
+git commit -m "first commit"
+git branch -M master
+git remote add origin https://github.com/santhu538/github-actions-example.git
+git push -u origin master
+     */
+}
---
+++ src/test/java/com/mycoding/cicd/GithubCicdActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycoding.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCicdActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
