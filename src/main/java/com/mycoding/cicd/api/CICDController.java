5ddc4dd771f9474b32c2cdf186efc1b10ae21765
Santhosh Kumar Boini <48869260+santhu538@users.noreply.github.com>
2023-04-22 23:10:38 -0400

    added query param

--- src/main/java/com/mycoding/cicd/api/CICDController.java
+++ src/main/java/com/mycoding/cicd/api/CICDController.java
@@ -1,13 +1,14 @@
 package com.mycoding.cicd.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/cicd")
 public class CICDController {
     @GetMapping("/welcome")
-    public String welcome() {
-        return "Welcome to Github Actions!!";
+    public String welcome(@RequestParam String name) {
+        return "Welcome to Github Actions!! "+name;
     }
     /*
     git init
